9f9129673e6fb3f092a00057e0d4a2b6e0016c39
Henry Williams <henry.williams@mastersny.org>
2016-07-26 11:05:42 -0400

    fixes

--- RNA-Seq/TCGA KIRC Methylation Analysis/main.R
+++ RNA-Seq/TCGA KIRC Methylation Analysis/main.R
@@ -19,21 +19,26 @@ main <- function()
 	{
 		for (patient in gene) 
 		{
+			TMatrix <- NULL
+			NTMatrix <- NULL
+			mvalues <- MValues(bvalues[probes(gene), patient])
+			
 			if (splitMatrix[patient, symbol]  == 1) 
 			{
-				TMatrix <- cbind(TMatrix, bvalues[probes(gene), patient])
+				TMatrix <- cbind(TMatrix, mvalues)
 			} 
 			else if (splitMatrix[patient, symbol] == 0)
 			{
-				NTMatrix <- cbind(NTMatrix, bvalues[probes(gene), patient])
+				NTMatrix <- cbind(NTMatrix, mvalues)
 			}
 		}
 
-		for (probe in rowNames(TMatrix)) a
+		for (probe in rowNames(TMatrix))
 		{
 			pvalues[probe] <- wilcox.test(TMatrix[probe], NTMatrix[probe], correct = FALSE)
-			adjpvalues(pvalue[probe], method = "BH")
-			sigprobes <- names(pvalues[which(adjpvalue < cutoff)])
+			adjpvalues <- p.adjust(pvalue[probe], method = "BH")
+			sigprobes <- names(pvalues[which(adjpvalues < cutoff)])
+			
 			write.table(c(gene, length/length(pvalues), sigprobes), append = TRUE)
 		}
 	}
