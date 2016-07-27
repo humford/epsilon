c17553351af889185207f28851e4da99def4f19b
benvchurch <benvoulgarischurch@gmail.com>
2016-07-27 12:59:25 -0400

    Update globals.R

--- RNA-Seq/TCGA KIRC Methylation Analysis/globals.R
+++ RNA-Seq/TCGA KIRC Methylation Analysis/globals.R
@@ -22,12 +22,6 @@ splitter <- function(exprs)
   }
 }
 
-mapper <- NULL
-
-probes <- function(gene, map = mapper)
-{
-  return(mapper[which(mapper[,"symbol"] == gene), "probe"])
-}
 
 TCGABarcode <- function(fileName)
 {
